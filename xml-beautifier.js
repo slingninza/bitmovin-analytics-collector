7f2a7cc5a251cceba934b7b29e7c7d50f56b10b7
Stephan Hesse <tchakabam@gmail.com>
2018-01-18 18:43:31 +0100

    fix lint errors

--- xml-beautifier.js
+++ xml-beautifier.js
@@ -7,7 +7,7 @@
  * @author Stephan Hesse <tchakabam@gmail.com>
  */
 
-const fs = require('fs')
+const fs = require('fs');
 const xmlBeautify = require('xml-beautifier');
 
 /*
@@ -17,38 +17,38 @@ const options = {
 }
 */
 
-let inputPath = process.argv[2]
-let outputPath = process.argv[3]
+let inputPath = process.argv[2];
+let outputPath = process.argv[3];
 
 if (typeof inputPath !== 'string') {
-  global.console.error('Usage: `xml-formatter.js inputfile [outputfile]`')
-  return
+  global.console.error('Usage: `<exec-command> inputfile [outputfile]`');
+  return;
 }
 
 fs.readFile(inputPath, {
   encoding: 'utf8'
 }, (err, inputXML) => {
-  if (err) throw err;
+  if (err) {throw err;}
 
-  const outputString = xmlBeautify(inputXML)
+  const outputString = xmlBeautify(inputXML);
 
   if (typeof outputPath !== 'string') {
-    process.stdout.write(outputString)
+    process.stdout.write(outputString);
 
-    process.exit(0)
-    return
+    process.exit(0);
+    return;
   }
 
   fs.writeFile(outputPath, outputString, {
     encoding: 'utf8'
   }, (err) => {
-    if (err) throw err;
+    if (err) {throw err;}
 
     global.console.log(`XML-beautifier: output has been written to ${outputPath}`);
 
-    process.exit(0)
-    return
-  })
-})
+    process.exit(0);
+    return;
+  });
+});
 
 
