022031bc6ae61fa9127c2812aee577b6c6895650
Stephan Hesse <tchakabam@gmail.com>
2018-01-19 00:24:12 +0100

    add mime types module

---
+++ js/enums/MIMETypes.js
@@ -0,0 +1,30 @@
+const MP4 = 'video/mp4';
+const WEBM = 'video/webm';
+const HLS = 'application/x-mpegURL';
+const DASH = 'application/dash+xml';
+
+export const MIMETypes = {
+  MP4,
+  WEBM,
+  HLS,
+  DASH
+};
+
+export function getMIMETypeFromFileExtension(path) {
+
+  path = path.toLowerCase();
+
+  if (path.endsWith('.m3u8')) {
+    return HLS;
+  }
+  if (path.endsWith('.mp4') || path.endsWith('.m4v') || path.endsWith('.m4a')) {
+    return MP4;
+  }
+  if (path.endsWith('.webm')) {
+    return WEBM;
+  }
+  if (path.endsWith('.mpd')) {
+    return DASH;
+  }
+  return null;
+}
