527292e9d1242048e451ad129037f77b2c4d998d
Stephan Hesse <tchakabam@gmail.com>
2018-01-18 16:27:49 +0100

    shaka adapter / state machine stubs

--- js/core/AnalyticsStateMachineFactory.js
+++ js/core/AnalyticsStateMachineFactory.js
@@ -1,19 +1,21 @@
-/**
- * Created by lkroepfl on 12.01.17.
- */
-
 import PlayerDetector from '../utils/PlayerDetector';
 import BitmovinAnalyticsStateMachine from '../analyticsStateMachines/BitmovinAnalyticsStateMachine';
 import Bitmovin7AnalyticsStateMachine from '../analyticsStateMachines/Bitmovin7AnalyticsStateMachine';
 import VideoJsAnalyticsStateMachine from '../analyticsStateMachines/VideoJsAnalyticsStateMachine';
 import HlsjsAnalyticsStateMachine from '../analyticsStateMachines/HlsjsAnalyticsStateMachine';
+import ShakaAnalyticsStateMachine from '../analyticsStateMachines/ShakaAnalyticsStateMachine'
 
+/**
+ * Stateless. Auto-maps given player instance to new state-machine instances.
+ * @class
+ */
 class AnalyticsStateMachineFactory {
-  constructor() {
-    this.playerDetector = new PlayerDetector;
-  }
-
-  getAnalyticsStateMachine(player, stateMachineCallbacks, opts = {}) {
+  /**
+   * @param {object} player
+   * @param {StateMchineCallbacks} stateMachineCallbacks
+   * @param {StateMachineOptions} opts
+   */
+  static getAnalyticsStateMachine(player, stateMachineCallbacks, opts = {}) {
     if (this.playerDetector.isBitmovinVersionPre7(player)) {
       return new BitmovinAnalyticsStateMachine(stateMachineCallbacks, opts);
     } else if (this.playerDetector.isBitmovinVersion7Plus(player)) {
@@ -22,6 +24,8 @@ class AnalyticsStateMachineFactory {
       return new VideoJsAnalyticsStateMachine(stateMachineCallbacks, opts);
     } else if (PlayerDetector.isHlsjs(player)) {
       return new HlsjsAnalyticsStateMachine(stateMachineCallbacks, opts);
+    } else if (PlayerDetector.isShaka(player)) {
+      return new ShakaAnalyticsStateMachine(stateMachineCallbacks, opts);
     }
   }
 }
