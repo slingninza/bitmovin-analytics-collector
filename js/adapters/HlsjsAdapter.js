d58130903cb6bbe41fab02e158fbce5e4d748839
Stephan Hesse <tchakabam@gmail.com>
2018-01-19 00:26:30 +0100

    break down hls and shaka adapters extending from html5 base

--- js/adapters/HlsjsAdapter.js
+++ js/adapters/HlsjsAdapter.js
@@ -2,42 +2,100 @@
  * @author Stephan Hesse <tchakabam@gmail.com>
  */
 
-import Events from '../enums/Events';
-
 import Hls from 'hls.js';
 
-const BUFFERING_TIMECHANGED_TIMEOUT = 1000;
+import {HTML5Adapter} from './HTML5Adapter';
+import {MIMETypes} from '../enums/MIMETypes';
 
-export default class HlsjsAdapter {
+/**
+ * @class
+ * @constructor
+ */
+export class HlsjsAdapter extends HTML5Adapter {
 
+  /**
+   * @constructs
+   * @param {Hls} hls
+   * @param {AnalyticsEventCallback} eventCallback
+   * @param {AnalyticsStateMachine} stateMachine
+   */
   constructor(hls, eventCallback, stateMachine) {
 
-    this.eventCallback = eventCallback;
-    this.stateMachine = stateMachine;
+    // we don't have a mediaEl yet per se
+    super(null, eventCallback, stateMachine);
+
+    /**
+     * @public
+     * @member {Hls} hls Hls.js client instance
+     */
     this.hls = hls;
 
-    // privates
-    this.analyticsBitrate_ = -1;
-    this.bufferingTimeout_ = null;
-    this.isBuffering_       = false;
-    this.isLive_ = false;
-    this.m3u8Url_ = null;
-    this.isPaused_ = false;
-    this.previousMediaTime_ = null;
-    this.needsReadyEvent_ = true;
-    this.needsFirstPlayIntent_ = true;
-
     this.resetMedia();
-    this.register();
+    this.registerHlsEvents();
   }
 
-  resetMedia() {
-    // HTMLMediaElement
-    this.mediaEl = null;
-    this.mediaElEventHandlers = [];
+  /**
+   * Implemented by sub-class to deliver current quality-level info
+   * specific to media-engine.
+   * @override
+   * @returns {QualityLevelInfo}
+   */
+  getCurrentQualityLevelInfo() {
+    const hls = this.hls;
+    const currentLevelObj = hls.levels[hls.currentLevel];
+    if (!currentLevelObj) {
+      return;
+    }
+
+    const attributes = currentLevelObj.attrs;
+    const bitrate = parseInt(attributes.BANDWIDTH, 10);
+    const width = parseInt(attributes.RESOLUTION.width, 10);
+    const height = parseInt(attributes.RESOLUTION.height, 10);
+
+    return {
+      bitrate,
+      width,
+      height
+    };
   }
 
-  register() {
+  /**
+   * @override
+   */
+  isLive() {
+    const hls = this.hls;
+    if (hls.currentLevel < 0) {
+      return false;
+    }
+    const currentLevelObj = hls.levels[hls.currentLevel];
+    if (!currentLevelObj) {
+      return false;
+    }
+    return currentLevelObj.details.live;
+  }
+
+  /**
+   * @override
+   */
+  getPlayerVersion() {
+    return Hls.version;
+  }
+
+  /**
+   * @override
+   */
+  getMIMEType() {
+    return MIMETypes.HLS;
+  }
+
+  /**
+   * @override
+   */
+  getStreamURL() {
+    return this.hls.url;
+  }
+
+  registerHlsEvents() {
     const hls = this.hls;
 
     if (!Hls) {
@@ -62,230 +120,6 @@ export default class HlsjsAdapter {
     }
   }
 
-  registerMediaElement() {
-
-    const mediaEl = this.mediaEl;
-    const hls = this.hls;
-
-    this.listenToMediaElementEvent('loadedmetadata', () => {
-
-      // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/readyState
-      // HAVE_NOTHING 0 No information is available about the media resource.
-      // HAVE_METADATA 1 Enough of the media resource has been retrieved that
-      // the metadata attributes are initialized. Seeking will no longer raise an exception.
-      // HAVE_CURRENT_DATA 2 Data is available for the current playback position,
-      // but not enough to actually play more than one frame.
-      // HAVE_FUTURE_DATA  3 Data for the current playback position as well as for
-      //  at least a little bit of time into the future is available
-      // (in other words, at least two frames of video, for example).
-      // HAVE_ENOUGH_DATA  4 Enough data is available—and the download rate is high
-      // enough—that the media can be played through to the end without interruption.
-      if (mediaEl.readyState !== 1) {
-        // we can't really gather any more information at this point
-        return;
-      }
-
-      // silent
-      this.checkQualityLevelAttributes(true);
-
-      const {duration, autoplay,
-        width, height,
-        videoWidth, videoHeight,
-        muted} = mediaEl;
-
-      // This is redundant with what we give to updateMetadata method.
-      // Not sure if there are good reasons to keep that so or if we should better centralize.
-      const info       = {
-        m3u8Url    : this.m3u8Url_,
-        isLive     : this.isLive_,
-        version    : Hls.version,
-        type       : 'html5',
-        streamType : 'hls',
-        duration,
-        autoplay,
-        // HTMLVideoElement.width and HTMLVideoElement.height
-        // is a DOMString that reflects the height HTML attribute,
-        // which specifies the height of the display area, in CSS pixels.
-        // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
-        width      : parseInt(width),
-        height     : parseInt(height),
-        // Returns an unsigned long containing the intrinsic
-        // height of the resource in CSS pixels,
-        // taking into account the dimensions, aspect ratio,
-        // clean aperture, resolution, and so forth,
-        // as defined for the format used by the resource.
-        // If the element's ready state is HAVE_NOTHING, the value is 0.
-        // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
-        videoWindowWidth : videoWidth,
-        videoWindowHeight: videoHeight,
-        muted
-      };
-
-      // silence events if we have not yet intended play
-      if (this.needsFirstPlayIntent_) {
-        return;
-      }
-
-      this.eventCallback(Events.METADATA_LOADED, info);
-    });
-
-    // We need the PLAY event to indicate the intent to play
-    // NOTE: use TIMECHANGED event on 'playing' and trigger PLAY as intended in states.dot graph
-
-    this.listenToMediaElementEvent('play', () => {
-      const {currentTime} = mediaEl;
-
-      this.needsFirstPlayIntent_ = false;
-
-      this.eventCallback(Events.PLAY, {
-        currentTime
-      });
-    });
-
-    this.listenToMediaElementEvent('pause', () => {
-      this.onPaused();
-    });
-
-    this.listenToMediaElementEvent('playing', () => {
-      const {currentTime} = mediaEl;
-
-      this.isPaused_ = false;
-
-      // silence events if we have not yet intended play
-      if (this.needsFirstPlayIntent_) {
-        return;
-      }
-
-      this.eventCallback(Events.TIMECHANGED, {
-        currentTime
-      });
-    });
-
-    this.listenToMediaElementEvent('error', () => {
-      const {currentTime, error} = mediaEl;
-
-      this.eventCallback(Events.ERROR, {
-        currentTime,
-        // See https://developer.mozilla.org/en-US/docs/Web/API/MediaError
-        code       : error.code,
-        message    : error.message
-      });
-    });
-
-    this.listenToMediaElementEvent('volumechange', () => {
-      const {muted, currentTime} = mediaEl;
-
-      if (muted) {
-        this.eventCallback(Events.MUTE, {
-          currentTime
-        });
-      } else {
-        this.eventCallback(Events.UN_MUTE, {
-          currentTime
-        });
-      }
-    });
-
-    this.listenToMediaElementEvent('seeking', () => {
-      const {currentTime} = mediaEl;
-
-      this.eventCallback(Events.SEEK, {
-        currentTime,
-        droppedFrames: 0
-      });
-    });
-
-    this.listenToMediaElementEvent('seeked', () => {
-      const {currentTime} = mediaEl;
-
-      if (this.bufferingTimeout_) {
-        clearTimeout(this.bufferingTimeout_);
-      }
-
-      this.eventCallback(Events.SEEKED, {
-        currentTime,
-        droppedFrames: 0
-      });
-    });
-
-    this.listenToMediaElementEvent('timeupdate', () => {
-
-      const {currentTime} = mediaEl;
-
-      this.isBuffering_ = false;
-
-      // silence events if we have not yet intended play
-      if (this.needsFirstPlayIntent_) {
-        return;
-      }
-
-      if (!this.isPaused_) {
-        this.eventCallback(Events.TIMECHANGED, {
-          currentTime
-        });
-      }
-
-      this.checkQualityLevelAttributes();
-
-      // We are doing this in case we can not rely
-      // on the "stalled" or "waiting" events in a specific browser
-      // and to detect intrinsinc paused states (when we do not get a paused event)
-      // but the player is paused already before attach or is paused from initialization on.
-      this.checkPlayheadProgress();
-
-      this.previousMediaTime_ = currentTime;
-    });
-
-    // The stalled event is fired when the user agent is trying to fetch media data,
-    // but data is unexpectedly not forthcoming.
-    // https://developer.mozilla.org/en-US/docs/Web/Events/stalled
-    this.listenToMediaElementEvent('stalled', () => {
-
-      // this event doesn't indicate buffering by definition (interupted playback),
-      // only that data throughput to playout buffers is not as high as expected
-      // It happens on Chrome every once in a while as SourceBuffer's are not fed
-      // as fast as the underlying native player may prefer (but it does not lead to
-      // interuption).
-    });
-
-    // The waiting event is fired when playback has stopped because of a temporary lack of data.
-    // See https://developer.mozilla.org/en-US/docs/Web/Events/waiting
-    this.listenToMediaElementEvent('waiting', () => {
-
-      this.onBuffering();
-    });
-
-  }
-
-  /**
-   * Should only be calld when a mediaEl is attached
-   */
-  unregisterMediaElement() {
-    if (!this.mediaEl) {
-      throw new Error('No media attached');
-    }
-
-    this.mediaElEventHandlers.forEach((handler) => {
-      this.mediaEl.removeEventListener(handler);
-    });
-
-    this.resetMedia();
-  }
-
-  /**
-   * Should only be calld when a mediaEl is attached
-   */
-  listenToMediaElementEvent(event, handler) {
-    if (!this.mediaEl) {
-      throw new Error('No media attached');
-    }
-
-    const boundHandler = handler.bind(this);
-
-    this.mediaElEventHandlers.push(boundHandler);
-    this.mediaEl.addEventListener(event, boundHandler, false);
-  }
-
   onMediaAttaching() {
     // in case we are called again (when we are triggering this ourselves
     // but from the event handler of MEDIA_ATTACHING) we should not run again.
@@ -293,10 +127,9 @@ export default class HlsjsAdapter {
       return;
     }
 
-    this.mediaEl = hls.media;
+    this.mediaEl = this.hls.media;
 
     this.registerMediaElement();
-
     this.onMaybeReady();
   }
 
@@ -305,147 +138,6 @@ export default class HlsjsAdapter {
   }
 
   onManifestLoading() {
-    // we don't care how often this gets called, its just a help-out
-    this.m3u8Url_ = this.hls.url;
-
     this.onMaybeReady();
-  }
-
-  onMaybeReady() {
-
-    if (!this.needsReadyEvent_ || !this.mediaEl || !this.m3u8Url_) {
-      return;
-    }
-
-    this.needsReadyEvent_ = false;
-
-    const {duration, autoplay,
-      width, height,
-      videoWidth, videoHeight,
-      muted} = this.mediaEl;
-
-    const info       = {
-      m3u8Url    : this.m3u8Url_,
-      isLive     : this.isLive_,
-      version    : Hls.version,
-      type       : 'html5',
-      streamType : 'hls',
-      duration   : duration,
-      autoplay   : autoplay,
-      // HTMLVideoElement.width and HTMLVideoElement.height
-      // is a DOMString that reflects the height HTML attribute,
-      // which specifies the height of the display area, in CSS pixels.
-      // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
-      width      : parseInt(width),
-      height     : parseInt(height),
-      // Returns an unsigned long containing the intrinsic
-      // height of the resource in CSS pixels,
-      // taking into account the dimensions, aspect ratio,
-      // clean aperture, resolution, and so forth,
-      // as defined for the format used by the resource.
-      // If the element's ready state is HAVE_NOTHING, the value is 0.
-      // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
-      videoWindowWidth : videoWidth,
-      videoWindowHeight: videoHeight,
-      muted
-    };
-
-    this.stateMachine.updateMetadata(info);
-
-    this.eventCallback(Events.READY, info);
-  }
-
-  onBuffering() {
-    const {currentTime} = this.mediaEl;
-
-    // this handler may be called multiple times
-    // for one actual buffering-event occuring so lets guard from
-    // triggering this event redundantly.
-    if (this.isBuffering_ || this.isPaused_) {
-      return;
-    }
-
-    this.eventCallback(Events.START_BUFFERING, {
-      currentTime
-    });
-
-    this.isBuffering_ = true;
-  }
-
-  onPaused() {
-    if (this.isPaused_) {
-      return;
-    }
-
-    const {currentTime} = this.mediaEl;
-
-    this.eventCallback(Events.PAUSE, {
-      currentTime
-    });
-
-    this.isPaused_ = true;
-  }
-
-  checkPlayheadProgress() {
-    const mediaEl = this.mediaEl;
-
-    if (mediaEl.paused) {
-      this.onPaused();
-    }
-
-    if (this.bufferingTimeout_) {
-      clearTimeout(this.bufferingTimeout_);
-    }
-
-    this.bufferingTimeout_ = window.setTimeout(() => {
-
-      if (mediaEl.paused || mediaEl.ended && !this.isBuffering_) {
-        return;
-      }
-
-      const timeDelta = mediaEl.currentTime - this.previousMediaTime_;
-
-      if (timeDelta < BUFFERING_TIMECHANGED_TIMEOUT) {
-        this.onBuffering();
-      }
-
-    }, BUFFERING_TIMECHANGED_TIMEOUT);
-  }
-
-  checkQualityLevelAttributes(silent = false) {
-
-    const mediaEl = this.mediaEl;
-    const hls = this.hls;
-
-    const currentLevelObj = hls.levels[hls.currentLevel];
-    if (!currentLevelObj) {
-      return;
-    }
-
-    const attributes   = currentLevelObj.attrs;
-    const bitrate      = attributes.BANDWIDTH;
-    const width        = attributes.RESOLUTION.width;
-    const height       = attributes.RESOLUTION.height;
-
-    const isLive = currentLevelObj.details.live;
-
-    if (isLive !== this.isLive_) {
-      this.isLive_ = isLive;
-      !silent && this.stateMachine.updateMetadata({
-        isLive
-      });
-    }
-
-    if (this.analyticsBitrate_ !== bitrate) {
-      const eventObject = {
-        width,
-        height,
-        bitrate: parseInt(bitrate, 10),
-        currentTime: mediaEl.currentTime
-      };
-
-      !silent && this.eventCallback(Events.VIDEO_CHANGE, eventObject);
-      this.analyticsBitrate_ = bitrate;
-    }
   }
 }
--- js/adapters/ShakaAdapter.js
+++ js/adapters/ShakaAdapter.js
@@ -2,383 +2,57 @@
  * @author Stephan Hesse <tchakabam@gmail.com>
  */
 
-import Events from '../enums/Events';
+/* global shaka */
 
-import Hls from 'hls.js';
+import {HTML5Adapter} from './HTML5Adapter';
+import {MIMETypes} from '../enums/MIMETypes';
 
-const BUFFERING_TIMECHANGED_TIMEOUT = 1000;
-
-export default class HlsjsAdapter {
+export class ShakaAdapter extends HTML5Adapter {
 
   constructor(shakaPlayer, eventCallback, stateMachine) {
-
-    /**
-     * @member {AnalyticsEventCallback}
-     */
-    this.eventCallback = eventCallback;
-
-    /**
-     * @member {AnalyticsStateMachine}
-     */
-    this.stateMachine = stateMachine;
-
-    /**
-     * @member {shaka.Player}
-     */
-    this.shakaPlayer = shakaPlayer;
-
-    /**
-     * @member {HTMLMediaElement}
-     */
-    this.mediaEl = null;
-
-    /**
-     * @member {function[]}
-     */
-    this.mediaElEventHandlers = [];
-
-    // privates
-    this.analyticsBitrate_ = -1;
-    this.bufferingTimeout_ = null;
-    this.isBuffering_ = false;
-    this.isLive_ = false;
-    this.isPaused_ = false;
-    this.previousMediaTime_ = null;
-    this.needsReadyEvent_ = true;
-    this.needsFirstPlayIntent_ = true;
-
-    this.register();
-    this.registerMediaElement();
-    this.onMaybeReady();
-  }
-
-  register() {
-    const shakaPlayer = this.shakaPlayer;
-
-    this.mediaEl = shakaPlayer.getMediaElement();
+    super(shakaPlayer.getMediaElement(), eventCallback, stateMachine);
 
     if (!shaka) {
       throw new Error('`shaka` lib is not installed (must be loaded before analytics module)');
     }
+
+    /**
+     * @public
+     * @member {shaka.Player}
+     */
+    this.shakaPlayer = shakaPlayer;
   }
 
-  registerMediaElement() {
+  getPlayerVersion() {
+    return shaka.Player.version;
+  }
 
-    const mediaEl = this.mediaEl;
-    const shakaPlayer = this.shakaPlayer;
-
-    this.listenToMediaElementEvent('loadedmetadata', () => {
-
-      // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/readyState
-      // HAVE_NOTHING 0 No information is available about the media resource.
-      // HAVE_METADATA 1 Enough of the media resource has been retrieved that
-      // the metadata attributes are initialized. Seeking will no longer raise an exception.
-      // HAVE_CURRENT_DATA 2 Data is available for the current playback position,
-      // but not enough to actually play more than one frame.
-      // HAVE_FUTURE_DATA  3 Data for the current playback position as well as for
-      //  at least a little bit of time into the future is available
-      // (in other words, at least two frames of video, for example).
-      // HAVE_ENOUGH_DATA  4 Enough data is available—and the download rate is high
-      // enough—that the media can be played through to the end without interruption.
-      if (mediaEl.readyState !== 1) {
-        // we can't really gather any more information at this point
-        return;
-      }
-
-      // silent
-      this.checkQualityLevelAttributes(true);
-
-      const {duration, autoplay,
-        width, height,
-        videoWidth, videoHeight,
-        muted} = mediaEl;
-
-      // This is redundant with what we give to updateMetadata method.
-      // Not sure if there are good reasons to keep that so or if we should better centralize.
-      const info       = {
-        isLive     : this.isLive_,
-        version    : shaka.Player.version,
-        type       : 'html5',
-        streamType : 'dash',
-        duration,
-        autoplay,
-        // HTMLVideoElement.width and HTMLVideoElement.height
-        // is a DOMString that reflects the height HTML attribute,
-        // which specifies the height of the display area, in CSS pixels.
-        // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
-        width      : parseInt(width),
-        height     : parseInt(height),
-        // Returns an unsigned long containing the intrinsic
-        // height of the resource in CSS pixels,
-        // taking into account the dimensions, aspect ratio,
-        // clean aperture, resolution, and so forth,
-        // as defined for the format used by the resource.
-        // If the element's ready state is HAVE_NOTHING, the value is 0.
-        // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
-        videoWindowWidth : videoWidth,
-        videoWindowHeight: videoHeight,
-        muted
-      };
-
-      // silence events if we have not yet intended play
-      if (this.needsFirstPlayIntent_) {
-        return;
-      }
-
-      this.eventCallback(Events.METADATA_LOADED, info);
-    });
-
-    // We need the PLAY event to indicate the intent to play
-    // NOTE: use TIMECHANGED event on 'playing' and trigger PLAY as intended in states.dot graph
-
-    this.listenToMediaElementEvent('play', () => {
-      const {currentTime} = mediaEl;
-
-      this.needsFirstPlayIntent_ = false;
-
-      this.eventCallback(Events.PLAY, {
-        currentTime
-      });
-    });
-
-    this.listenToMediaElementEvent('pause', () => {
-      this.onPaused();
-    });
-
-    this.listenToMediaElementEvent('playing', () => {
-      const {currentTime} = mediaEl;
-
-      this.isPaused_ = false;
-
-      // silence events if we have not yet intended play
-      if (this.needsFirstPlayIntent_) {
-        return;
-      }
-
-      this.eventCallback(Events.TIMECHANGED, {
-        currentTime
-      });
-    });
-
-    this.listenToMediaElementEvent('error', () => {
-      const {currentTime, error} = mediaEl;
-
-      this.eventCallback(Events.ERROR, {
-        currentTime,
-        // See https://developer.mozilla.org/en-US/docs/Web/API/MediaError
-        code       : error.code,
-        message    : error.message
-      });
-    });
-
-    this.listenToMediaElementEvent('volumechange', () => {
-      const {muted, currentTime} = mediaEl;
-
-      if (muted) {
-        this.eventCallback(Events.MUTE, {
-          currentTime
-        });
-      } else {
-        this.eventCallback(Events.UN_MUTE, {
-          currentTime
-        });
-      }
-    });
-
-    this.listenToMediaElementEvent('seeking', () => {
-      const {currentTime} = mediaEl;
-
-      this.eventCallback(Events.SEEK, {
-        currentTime,
-        droppedFrames: 0
-      });
-    });
-
-    this.listenToMediaElementEvent('seeked', () => {
-      const {currentTime} = mediaEl;
-
-      if (this.bufferingTimeout_) {
-        clearTimeout(this.bufferingTimeout_);
-      }
-
-      this.eventCallback(Events.SEEKED, {
-        currentTime,
-        droppedFrames: 0
-      });
-    });
-
-    this.listenToMediaElementEvent('timeupdate', () => {
-
-      const {currentTime} = mediaEl;
-
-      this.isBuffering_ = false;
-
-      // silence events if we have not yet intended play
-      if (this.needsFirstPlayIntent_) {
-        return;
-      }
-
-      if (!this.isPaused_) {
-        this.eventCallback(Events.TIMECHANGED, {
-          currentTime
-        });
-      }
-
-      this.checkQualityLevelAttributes();
-
-      // We are doing this in case we can not rely
-      // on the "stalled" or "waiting" events in a specific browser
-      // and to detect intrinsinc paused states (when we do not get a paused event)
-      // but the player is paused already before attach or is paused from initialization on.
-      this.checkPlayheadProgress();
-
-      this.previousMediaTime_ = currentTime;
-    });
-
-    // The stalled event is fired when the user agent is trying to fetch media data,
-    // but data is unexpectedly not forthcoming.
-    // https://developer.mozilla.org/en-US/docs/Web/Events/stalled
-    this.listenToMediaElementEvent('stalled', () => {
-
-      // this event doesn't indicate buffering by definition (interupted playback),
-      // only that data throughput to playout buffers is not as high as expected
-      // It happens on Chrome every once in a while as SourceBuffer's are not fed
-      // as fast as the underlying native player may prefer (but it does not lead to
-      // interuption).
-    });
-
-    // The waiting event is fired when playback has stopped because of a temporary lack of data.
-    // See https://developer.mozilla.org/en-US/docs/Web/Events/waiting
-    this.listenToMediaElementEvent('waiting', () => {
-
-      this.onBuffering();
-    });
-
+  isLive() {
+    return this.shakaPlayer ? this.shakaPlayer.isLive() : false;
   }
 
   /**
-   * Should only be calld when a mediaEl is attached
+   * @override
    */
-  listenToMediaElementEvent(event, handler) {
-    if (!this.mediaEl) {
-      throw new Error('No media attached');
-    }
-
-    const boundHandler = handler.bind(this);
-
-    this.mediaElEventHandlers.push(boundHandler);
-    this.mediaEl.addEventListener(event, boundHandler, false);
+  getMIMEType() {
+    // FIXME: Could be HLS too, Shaka probably has a method to find out
+    return MIMETypes.DASH;
   }
 
-  onMaybeReady() {
-
-    if (!this.needsReadyEvent_ || !this.mediaEl) {
-      return;
-    }
-
-    this.needsReadyEvent_ = false;
-
-    const {
-      duration,
-      autoplay,
-      width,
-      height,
-      videoWidth,
-      videoHeight,
-      muted
-    } = this.mediaEl;
-
-    const info       = {
-      isLive     : this.isLive_,
-      version    : shaka.Player.version,
-      type       : 'html5',
-      streamType : 'dash',
-      duration   : duration,
-      autoplay   : autoplay,
-      // HTMLVideoElement.width and HTMLVideoElement.height
-      // is a DOMString that reflects the height HTML attribute,
-      // which specifies the height of the display area, in CSS pixels.
-      // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
-      width      : parseInt(width),
-      height     : parseInt(height),
-      // Returns an unsigned long containing the intrinsic
-      // height of the resource in CSS pixels,
-      // taking into account the dimensions, aspect ratio,
-      // clean aperture, resolution, and so forth,
-      // as defined for the format used by the resource.
-      // If the element's ready state is HAVE_NOTHING, the value is 0.
-      // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
-      videoWindowWidth : videoWidth,
-      videoWindowHeight: videoHeight,
-      muted
-    };
-
-    this.stateMachine.updateMetadata(info);
-
-    this.eventCallback(Events.READY, info);
+  /**
+   * @override
+   */
+  getStreamURL() {
+    return this.shakaPlayer ? this.shakaPlayer.getManifestUri() : null;
   }
 
-  onBuffering() {
-    const {currentTime} = this.mediaEl;
-
-    // this handler may be called multiple times
-    // for one actual buffering-event occuring so lets guard from
-    // triggering this event redundantly.
-    if (this.isBuffering_ || this.isPaused_) {
-      return;
-    }
-
-    this.eventCallback(Events.START_BUFFERING, {
-      currentTime
-    });
-
-    this.isBuffering_ = true;
-  }
-
-  onPaused() {
-    if (this.isPaused_) {
-      return;
-    }
-
-    const {currentTime} = this.mediaEl;
-
-    this.eventCallback(Events.PAUSE, {
-      currentTime
-    });
-
-    this.isPaused_ = true;
-  }
-
-  checkPlayheadProgress() {
-    const mediaEl = this.mediaEl;
-
-    if (mediaEl.paused) {
-      this.onPaused();
-    }
-
-    if (this.bufferingTimeout_) {
-      clearTimeout(this.bufferingTimeout_);
-    }
-
-    this.bufferingTimeout_ = window.setTimeout(() => {
-
-      if (mediaEl.paused || mediaEl.ended && !this.isBuffering_) {
-        return;
-      }
-
-      const timeDelta = mediaEl.currentTime - this.previousMediaTime_;
-
-      if (timeDelta < BUFFERING_TIMECHANGED_TIMEOUT) {
-        this.onBuffering();
-      }
-
-    }, BUFFERING_TIMECHANGED_TIMEOUT);
-  }
-
-  checkQualityLevelAttributes(silent = false) {
-
-    const mediaEl = this.mediaEl;
-
+  /**
+   * Implemented by sub-class to deliver current quality-level info
+   * specific to media-engine.
+   * @override
+   * @returns {QualityLevelInfo}
+   */
+  getCurrentQualityLevelInfo() {
     const variantTracks = this.shakaPlayer.getVariantTracks();
 
     const activeVideoTrack = variantTracks
@@ -387,34 +61,17 @@ export default class HlsjsAdapter {
 
     if (!activeVideoTrack) {
       // can only happen for audio-only streams
-      return;
+      return null;
     }
 
-    const bitrate      = activeVideoTrack.videoBandwidth || activeVideoTrack.bandwidth;
-    const width        = activeVideoTrack.width;
-    const height       = activeVideoTrack.height;
+    const bitrate = activeVideoTrack.videoBandwidth || activeVideoTrack.bandwidth;
+    const width = activeVideoTrack.width;
+    const height = activeVideoTrack.height;
 
-    const isLive = this.shakaPlayer.isLive();
-
-    if (isLive !== this.isLive_) {
-      this.isLive_ = isLive;
-      !silent && this.stateMachine.updateMetadata({
-        isLive
-      });
-    }
-
-    if (this.analyticsBitrate_ !== bitrate) {
-      const eventObject = {
-        width,
-        height,
-        bitrate: parseInt(bitrate, 10),
-        currentTime: mediaEl.currentTime
-      };
-
-      //console.log(eventObject)
-
-      !silent && this.eventCallback(Events.VIDEO_CHANGE, eventObject);
-      this.analyticsBitrate_ = bitrate;
-    }
+    return {
+      bitrate,
+      width,
+      height
+    };
   }
 }
