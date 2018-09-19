e7a10d2072ee3620ea8b86dd97240fd76fb29a06
Peter Pfaffeneder <peter.pfaffeneder@gmail.com>
2018-09-19 09:54:54 +0200

    Fixed imports

--- js/types/ads/AdSample.ts
+++ js/types/ads/AdSample.ts
@@ -1,5 +1,5 @@
-import { StrategyType } from '../../enums/ad/StrategyType';
-import { CreativeType } from '../../enums/ad/CreativeType';
+import { StrategyType } from '../../enums/ads/StrategyType';
+import { CreativeType } from '../../enums/ads/CreativeType';
 
 export interface AdSample {
   adImpressionId?: string;
